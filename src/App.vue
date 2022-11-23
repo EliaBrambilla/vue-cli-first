b2bf4c3fc2e5f1dbe92c87ed6d1e96a56580d178
EliaBrambilla <elia.brambilla@icloud.com>
2022-11-23 01:37:33 +0100

    aggiungo prova hello vue.js

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>La mia prima app con Vue CLI!</h1>
+    Ciao a tutti
+    <p>{{ saluto }}</p>
+    <div v-for="(item, index) in test" :key="index">s
+      {{ item }}
+      <FirstComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import FirstComponent from './components/first-component.vue';
+export default {
+  name: 'App',
+  data() {
+    return {
+      saluto: `Hello World`,
+      test: [1, 4, 8, 12, 15]
+    }
+  },
+  components: {
+    FirstComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/first-component.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="red" @click="saluta">
+      
+      <div class="inner">Sono Elia</div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstComponent',
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    saluta() {
+      console.log('ti saluta il tuo primo componente');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$bgColor: red;
+@mixin ac-square {
+    width: 150px;
+    height: 150px;
+}
+.red-square{
+    @include ac-square();
+    background-color: brown;
+}
+/*
+.red {
+   background-color: $bgColor;
+    .inner{
+        background-color: yellowgreen;
+    }
+    &.active{
+        background-color: blue;
+    }
+}
+.inner{
+    background-color: yellow;
+}
+*/
+// il preprocessore trasforma il scss in css standard:
+.red {
+    background-color: red;
+}
+.red .inner{
+    background-color: yellowgreen;
+}
+.red.active{
+    background-color: blue;
+}
+.inner{
+     background-color: yellow;
+}
+</style>
